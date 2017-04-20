5df39347b2c28e7c0b6dc2e276a4a16b0dd02fd9
frickiericker <frickiericker@gmail.com>
2017-04-21 02:17:55 +0900

    Make interval hard-coded temporarily

--- hackernewsbot.py
+++ hackernewsbot.py
@@ -92,8 +92,7 @@ class StorySubmitter(object):
 
     async def submit_stories(self):
         with self._database.cursor() as cursor:
-            cursor.execute('SELECT * FROM stories WHERE time < NOW() - INTERVAL %s;',
-                           (self._hold_time, ))
+            cursor.execute("SELECT * FROM stories WHERE time < NOW() - INTERVAL '30 minutes';")
             LOG.debug('{} stories to submit'.format(cursor.rownumber))
 
 class Story(object):
