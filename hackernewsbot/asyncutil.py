82bedb053e023736c1a98fd7ea4de6eca81042c0
frickiericker <frickiericker@gmail.com>
2017-04-21 06:20:06 +0900

    Fix typo

--- hackernewsbot/asyncutil.py
+++ hackernewsbot/asyncutil.py
@@ -1,4 +1,4 @@
 import asyncio
 
 def do_async(func, *args):
-    return asyncio.get_event_loop().run_in_executer(None, func, *args)
+    return asyncio.get_event_loop().run_in_executor(None, func, *args)
