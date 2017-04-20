01e8f552dfd78bd244cf44901eb2bb2301c8a376
frickiericker <frickiericker@gmail.com>
2017-04-21 06:56:07 +0900

    Make web app to show database status

--- statuspage/app.py
+++ statuspage/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
+import os
+import psycopg2
+
+DATABASE_URL = os.environ.get('DATABASE_URL', None)
 
 app = Flask(__name__)
 
 @app.route('/')
 def top_page():
-    return 'Alive'
+    with psycopg2.connect(DATABASE_URL) as database:
+        return get_database_status(database)
+
+def get_database_status(database):
+    stories = get_story_statuses()
+    num_processed = sum(1 for ident, serial, processed if processed)
+    return '{} stories, {} processed'.format(len(stories), num_processed)
+
+def get_story_statuses(database):
+    with database.cursor() as cursor:
+        cursor.execute('SELECT * FROM processingStatus;')
+        return cursor.fetchall()
