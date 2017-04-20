4d19ec8a2d5ea7bd12efe8320c9864bfcf06bda8
frickiericker <frickiericker@gmail.com>
2017-04-21 03:43:02 +0900

    Change parameter names

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ from hackernewsbot.dbcleaner import DatabaseCleaner
 DATABASE_URL = os.environ.get('DATABASE_URL', None)
 COLLECTOR_SLEEP = int(os.environ.get('COLLECTOR_SLEEP', 300))
 BOTPOSTER_SLEEP = int(os.environ.get('BOTPOSTER_SLEEP', 10))
-DBCLEANER_SLEEP = int(os.environ.get('DBCLEANER_SLEEP', 100))
-BOTPOST_HOLD_TIME = int(os.environ.get('BOTPOST_HOLD_TIME', 60)) # minutes
+DBCLEANER_SLEEP = int(os.environ.get('DBCLEANER_SLEEP', 500))
+STORY_WARMUP = int(os.environ.get('STORY_WARMUP', 60 * 60))
 STORY_TTL = int(os.environ.get('STORY_TTL', 12 * 60 * 60))
 
 def main():
@@ -31,7 +31,7 @@ def make_collector(story_database):
     return StoryCollector(story_database)
 
 def make_botposter(story_database):
-    return StoryPoster(story_database, timedelta(minutes=BOTPOST_HOLD_TIME))
+    return StoryPoster(story_database, timedelta(seconds=STORY_WARMUP))
 
 def make_dbcleaner(story_database):
     return DatabaseCleaner(story_database, timedelta(seconds=STORY_TTL))
