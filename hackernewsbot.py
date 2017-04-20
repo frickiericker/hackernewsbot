fa89d704e508ce09413e07783e523e609dc5d3d7
frickiericker <frickiericker@gmail.com>
2017-04-21 02:42:13 +0900

    Change default hold time to 60 minutes

--- hackernewsbot.py
+++ hackernewsbot.py
@@ -11,7 +11,7 @@ import requests
 DATABASE_URL = os.environ.get('DATABASE_URL', None)
 COLLECTOR_SLEEP = os.environ.get('COLLECTOR_SLEEP', 300)
 BOTPOSTER_SLEEP = os.environ.get('BOTPOSTER_SLEEP', 10)
-BOTPOST_HOLD_TIME = os.environ.get('BOTPOST_HOLD_TIME', 30) # minutes
+BOTPOST_HOLD_TIME = os.environ.get('BOTPOST_HOLD_TIME', 60) # minutes
 
 LOG = logging.getLogger(__name__)
 LOG.addHandler(logging.StreamHandler())
