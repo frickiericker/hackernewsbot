33069f6f37c67ca33d2deda5ba27862f0b435841
frickiericker <frickiericker@gmail.com>
2017-04-21 16:16:41 +0900

    Change DB facade to reconnect to DB on every action

--- storydb/storydb.py
+++ storydb/storydb.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 from urllib.parse import urlparse
 import psycopg2
 
-def connect_to_postgres(url):
+def _connect(url):
     url = urlparse(url)
     connection = psycopg2.connect(
         database=url.path[1:],
@@ -15,10 +15,10 @@ def connect_to_postgres(url):
 
 class StoryRepository:
     def __init__(self, url):
-        self._database = connect_to_postgres(url)
+        self._url = url
 
     def insert_story(self, story_id, submission_time):
-        with self._database.cursor() as cursor:
+        with _connect(self._url) as database, database.cursor() as cursor:
             cursor.execute("""
                 insert into story (id) values (%(id)s) returning index;
             """, {'id': story_id})
@@ -27,27 +27,27 @@ class StoryRepository:
                 insert into story_submission_time values (%(index)s, %(time)s);
                 insert into story_processing_status values (%(index)s, false);
             """, {'index': index, 'time': submission_time})
-        self._database.commit()
+            database.commit()
 
     def has_story(self, story_id):
-        with self._database.cursor() as cursor:
+        with _connect(self._url) as database, database.cursor() as cursor:
             cursor.execute("""
                 select * from story where id = %(id)s;
             """, {'id': story_id})
             return cursor.fetchone() is not None
 
     def mark_story(self, story_id, processed=True):
-        with self._database.cursor() as cursor:
+        with _connect(self._url) as database, database.cursor() as cursor:
             cursor.execute("""
                 update story_processing_status set processed = %(processed)s
                        from story
                        where story.index = story_processing_status.index and
                              story.id = %(id)s;
             """, {'id': story_id, 'processed': processed})
-        self._database.commit()
+            database.commit()
 
     def delete_stale_stories(self, numkept):
-        with self._database.cursor() as cursor:
+        with _connect(self._url) as database, database.cursor() as cursor:
             cursor.execute("""
                 select max(index) from story;
             """)
@@ -60,18 +60,17 @@ class StoryRepository:
                 delete from story_submission_time where index <= %(threshold)s;
                 delete from story_processing_status where index <= %(threshold)s;
             """, {'threshold': threshold})
-        self._database.commit()
+        database.commit()
 
     def get_stored_stories(self):
-        with self._database.cursor() as cursor:
+        with _connect(self._url) as database, database.cursor() as cursor:
             cursor.execute("""
                 select id from story
             """)
             return [story_id for story_id, in cursor]
 
     def get_pending_stories(self, age=timedelta(0)):
-        print('get_pending_stories {}'.format(age))
-        with self._database.cursor() as cursor:
+        with _connect(self._url) as database, database.cursor() as cursor:
             cursor.execute("""
                 select story.id
                        from story, story_submission_time, story_processing_status
