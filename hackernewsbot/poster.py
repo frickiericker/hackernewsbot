e05fff954223fd6cbb1f567a35e380464211394c
frickiericker <frickiericker@gmail.com>
2017-04-21 23:28:00 +0900

    Change toot format

--- hackernewsbot/poster.py
+++ hackernewsbot/poster.py
@@ -5,6 +5,15 @@ import requests
 
 from appenv import MASTODON_TIMEOUT
 
+MESSAGE_TEMPLATE = '{title}\n{uri}\n\n{score} | {comments}\n({age})'
+
+def plural(number, thing):
+    if number == 1:
+        return '{} {}'.format(number, thing)
+    else:
+        return '{} {}s'.format(number, thing)
+
+
 class MastodonPoster:
     def __init__(self, instance, client_id, client_secret, email, password):
         self._instance = instance
@@ -22,7 +31,6 @@ class MastodonPoster:
         response_data = json.loads(response.text)
         self._access_token = response_data['access_token']
         self._token_type = response_data['token_type']
-        logging.info('auth: {}'.format(response.text))
         response.raise_for_status()
 
     async def post(self, story):
@@ -32,10 +40,12 @@ class MastodonPoster:
         now = datetime.now(timezone.utc)
         age = now - story.time
         age_in_minutes = round(age.total_seconds() / 60)
-        hackernews_uri = 'https://news.ycombinator.com/item?id={}'.format(story.id)
-        text = '{}\n{}\n\n{} comments {} points\n(in {} minutes)'.format(
-            story.title, hackernews_uri, len(story.comments), story.score,
-            age_in_minutes
+        text = MESSAGE_TEMPLATE.format(
+            title=story.title,
+            uri='https://news.ycombinator.com/item?id={}'.format(story.id),
+            score=plural(story.score, 'point'),
+            comments=plural(len(story.comments), 'comment'),
+            age='in {} minutes'.format(age_in_minutes)
         )
         response = requests.post(self._instance + '/api/v1/statuses', {
             'status': text,
@@ -43,5 +53,4 @@ class MastodonPoster:
         }, headers={
             'Authorization': '{} {}'.format(self._token_type, self._access_token)
         }, timeout=MASTODON_TIMEOUT)
-        logging.info('response: ' + response.text)
         response.raise_for_status()
