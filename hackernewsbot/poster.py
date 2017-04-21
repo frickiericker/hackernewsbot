f2982dc48ac4552057125fcd6fcf1d9a5987e56c
frickiericker <frickiericker@gmail.com>
2017-04-21 17:26:46 +0900

    Add stub oauth

--- hackernewsbot/poster.py
+++ hackernewsbot/poster.py
@@ -1,10 +1,27 @@
 import logging
+import json
 import requests
 
 class MastodonPoster:
-    def __init__(self, **options):
-        pass
+    def __init__(self, instance, client_id, client_secret, email, password):
+        self._instance = instance
+        self._authenticate(client_id, client_secret, email, password)
 
+    def _authenticate(self, client_id, client_secret, email, password):
+        uri = instance + '/oauth/token'
+        response = requests.post(uri, {
+            'grant_type': 'password',
+            'scope': 'write',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'username': email,
+            'password': password
+        })
+        print(response.text)
+
+#curl -X POST \
+#     -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=password&username=${EMAIL}&password=${PASSWORD}" \
+#     -Ss "${INSTANCE}/oauth/token"
     async def post(self, story):
         logging.info('posting {} | {}-{} | {}'.format(
             story.id, len(story.comments), story.score, story.title
