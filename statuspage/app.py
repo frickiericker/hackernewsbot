acedeef1175e5f6f33bdf66c16a2fa022cc8fb92
frickiericker <frickiericker@gmail.com>
2017-05-07 03:54:41 +0900

    Remote dead code

--- statuspage/app.py
+++ statuspage/app.py
@@ -12,11 +12,6 @@ app = Flask(__name__)
 def top_page():
     return report_processing_status(StoryRepository(DATABASE_URL))
 
-def get_database_status(database):
-    stories = get_story_statuses(database)
-    num_processed = sum(1 for ident, serial, processed in stories if processed)
-    return '{} stories, {} processed'.format(len(stories), num_processed)
-
 def report_processing_status(repository):
     num_stories = len(repository.get_stored_stories())
     num_pending = len(repository.get_pending_stories())
